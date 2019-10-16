ac3c1901665aef4761e5eca518b54baa95aa3147
Sam Fortin <sam.j.fortin@gmail.com>
2019-10-16 16:05:54 -0400

    bringing in tailwind css

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,25 +1,49 @@
 <template>
-  <div class="hello">
-    <img alt="sjf logo" src="../assets/head.jpg">
-    <h1>{{ msg }}</h1>
-    <p>Web Developer from Minneapolis. Enjoying the never ending learning and new things to do in the world of web dev.</p>
-    <ul>
-      <li><a href="mailto:sam.j.fortin@gmail.com" target="_blank" rel="noopener">gmail</a></li>
-      <li><a href="https://github.com/sjfortin" target="_blank" rel="noopener">github</a></li>
-      <li><a href="https://www.linkedin.com/in/samfortin/" target="_blank" rel="noopener">linkedin</a></li>
-      <li><a href="https://codepen.io/sjfortin/" target="_blank" rel="noopener">codepen</a></li>
-      <li><a href="https://sjfortin.com" target="_blank" rel="noopener">portfolio of some work</a></li>
+  <div class="hello container mx-auto text-center">
+    <img class="mx-auto" alt="sjf logo" src="../assets/head.jpeg" />
+    <h1 class="text-4xl">{{ msg }}</h1>
+    <p>Web Developer from Minneapolis.</p>
+    <ul class="my-4">
+      <li class="inline mx-2" v-for="link in links" v-bind:key="link.name">
+        <a class="text-purple-500 hover:text-purple-700 focus:text-purple-700" v-bind:href="link.href" target="_blank" rel="noopener">{{link.name}}</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "LandingPage",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      links: [
+        {
+          name: "gmail",
+          href: "mailto:sam.j.fortin@gmail.com"
+        },
+        {
+          name: "github",
+          href: "https://github.com/sjfortin"
+        },
+        {
+          name: "linked",
+          href: "https://linkedin.com/sjfortin"
+        },
+        {
+          name: "codepen",
+          href: "https://codepen.io/sjfortin/"
+        },
+        {
+          name: "(old) portfolio",
+          href: "https://sjfortin.com"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -28,22 +52,5 @@ img {
   border-radius: 50%;
   width: 140px;
   height: 140px;
-}
-h1 {
-  margin-top: 0;  
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
